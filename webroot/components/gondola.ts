fd80afcdc7a3788dbe0a85a8eace7d502104498d
Seth Sandor <Seth.sandor@ge.com>
2016-03-02 18:06:35 -0500

    extra fiddly bits to get text templates functional using system syntax

---
+++ webroot/components/gondola.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+import tpl from './gondola.html!text';
+
+@Component({
+    selector: 'my-gondola',
+    template: tpl
+})
+
+export class Gondola { }
--- webroot/main.ts
+++ webroot/main.ts
@@ -1,4 +1,4 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './components/body';
+import {Gondola} from './components/gondola';
 
-bootstrap(AppComponent);
+bootstrap(Gondola);
