68ecec415ba5eabc88016ee5277e571de4089bc1
Seth Sandor <Seth.Sandor@ge.com>
2016-04-04 16:57:17 -0400

    More placeholder stuff

--- webroot/components/header/header.ts
+++ webroot/components/header/header.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
 import headerTmpl from './header.html';
+import headerStyles from './header.css';
 
 @Component({
     selector: 'my-header',
+    styles: [headerStyles],
     template: headerTmpl
 })
 
