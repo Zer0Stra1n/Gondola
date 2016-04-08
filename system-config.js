a213ff56857801aca507d5d308b753ae48452e42
Seth Sandor <Seth.Sandor@ge.com>
2016-04-08 17:12:14 -0400

    new bundle

--- system-config.js
+++ system-config.js
@@ -1,7 +1,7 @@
 System.config({
     map: {
         'text': 'node_modules/systemjs-plugin-text/text.js'
-        // 'boot': 'webroot/boot.min.js'    
+        // 'boot': 'webroot/boot.min.js'
     },
     packages: {
         'webroot': {
@@ -19,7 +19,18 @@ System.config({
     }
     // bundles: {
     //     'boot': ['webroot/boot.js',
-    //              'webroot/components/gondola.js',
-    //              'webroot/components/gondola.html']
+    //         'webroot/components/gondola.js',
+    //         'webroot/components/header/header.js',
+    //         'webroot/components/archive_view/archive_view.js',
+    //         'webroot/components/footer/footer.js',
+    //         'webroot/components/gondola.html',
+    //         'webroot/components/archive_view/add_card/add_card.js',
+    //         'webroot/components/header/header.css',
+    //         'webroot/components/header/header.html',
+    //         'webroot/components/archive_view/archive_view.html',
+    //         'webroot/components/footer/footer.html',
+    //         'webroot/components/footer/footer.css',
+    //         'webroot/components/archive_view/add_card/add_card.css',
+    //         'webroot/components/archive_view/add_card/add_card.html']
     // }
 });
