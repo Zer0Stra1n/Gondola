25e2d2742e2121a678eddda75f85e2d19a4c1835
Seth Sandor <seth.sandor@ge.com>
2016-05-24 17:38:01 -0400

    cleaned up the builder. Should be able to trace history using Git

--- builder.js
+++ builder.js
@@ -3,11 +3,6 @@ var Builder = require('systemjs-builder');
 // optional constructor options
 // sets the baseURL and loads the configuration file
 var builder = new Builder('.', 'webroot/system.config.js');
-builder.config({
-    // paths: {
-    //     '@angular':  'node_modules/@angular/'
-    // },    
-});
 
 builder.bundle('webroot/boot.js - [webroot/**/*]', 'webroot/dependencies.min.js',
     {
