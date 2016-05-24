f72ef6475001b4955e8167105c6143aad8a5f229
Seth Sandor <seth.sandor@ge.com>
2016-05-24 17:36:36 -0400

    better builder

--- builder.js
+++ builder.js
@@ -16,27 +16,21 @@ builder.bundle('webroot/boot.js - [webroot/**/*]', 'webroot/dependencies.min.js'
         mangle: true,
         globalDefs: { DEBUG: false }
     }
-).then(function(output) {
-  console.log('Angular Build complete');
-  console.log(output.modules);
-})
-.catch(function(err) {
-  console.log('Build error');
-  console.log(err);
-});
-
-builder.bundle('webroot/boot.js - webroot/dependencies.min.js', 'webroot/boot.min.js', 
-    {
-        normalize: true,
-        minify: true,
-        mangle: false,
-        globalDefs: { DEBUG: false }
-    })
-.then(function(output) {
-  console.log('App Build complete');
-  console.log(output.modules);
-})
-.catch(function(err) {
-  console.log('Build error');
-  console.log(err);
+).then(deps => {
+    console.log('Angular Build complete');
+    console.log(deps.modules);
+}).then(() => {
+    return builder.bundle('webroot/boot.js - webroot/dependencies.min.js', 'webroot/boot.min.js',
+        {
+            normalize: true,
+            minify: true,
+            mangle: false,
+            globalDefs: { DEBUG: false }
+        })
+}).then(app => {
+    console.log('App Build complete');
+    console.log(app.modules);
+}).catch(err => {
+    console.log('Build error');
+    console.log(err);
 });
